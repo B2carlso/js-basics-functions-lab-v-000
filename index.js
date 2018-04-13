2b46050c1816c796894cc49c8b43a7201e3fc01a
Brad Carlson <b2carlso@yahoo.com>
2018-04-13 19:01:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function calculatesFarePrice(startingBlock, endingBlock) {
 	else if (distance > 400 && distance <= 2000) {
 		return distance * .02 }
   else if (distance > 2000 && distance < 2500) {
-    return distance * 25}
+    return 25}
 	else if (distance > 2500) {
 		return 'cannot travel that far'
     }
